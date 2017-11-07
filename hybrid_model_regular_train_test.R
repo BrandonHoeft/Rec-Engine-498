dee626737ba4ed045897b09ad190ebb2a5cf049e
BrandonHoeft <brandonmhoeft@gmail.com>
2017-11-07 03:36:43 +0000

    Updated Hybrid2 model: UBCF and Latent Factor model with predictions dataframe.

--- hybrid_model_regular_train_test.R
+++ hybrid_model_regular_train_test.R
@@ -226,9 +226,11 @@ calcPredictionAccuracy(hybrid_predictions,
                        goodRating = 3,
                        byUser = FALSE)
 
-# Build a hybrid UBCF recommender with a implicit rating latent factor model ---------
 
-hybrid_rec_model <- HybridRecommender(
+
+# Build a hybrid UBCF recommender with a implicit rating latent factor model (BEST) ---------
+
+hybrid_rec_model2 <- HybridRecommender(
   Recommender(getData(train_scheme, "train"), 
               method = "UBCF", 
               parameter = list(method = "cosine", 
@@ -236,6 +238,167 @@ hybrid_rec_model <- HybridRecommender(
                                normalize = "center")),
   Recommender(getData(train_scheme, "train"), 
               method = "ALS_implicit",
-              parameter = NULL), # default parameters
+              parameter = list(lambda = 0.1,
+                               alpha = 10,
+                               n_factors = 100,
+                               n_iterations = 10,
+                               min_item_nr = 1,
+                               seed = 2017)), # default parameters
   weights = c(.5, .5)
 )
+
+
+# Fit testing records.
+hybrid2_predictions <- predict(hybrid_rec_model2, 
+                              getData(train_scheme, "known"), 
+                              type = "topNList", 
+                              n = 20)
+s3save(hybrid2_predictions, bucket = "pred498finalmodel", object = "hybrid2_predictions.Rdata")
+#s3load("hybrid2_predictions.Rdata", bucket = "pred498finalmodel")
+
+str(hybrid2_predictions)
+hybrid2_accuracy_by_user <- calcPredictionAccuracy(hybrid2_predictions,
+                                                  getData(train_scheme, "unknown"),
+                                                  given = 5,
+                                                  goodRating = 3,
+                                                  byUser = TRUE)
+
+hybrid2_accuracy_by_user <- as.data.frame(hybrid2_accuracy_by_user) %>%
+  mutate(VisitorId = row.names(.)) %>%
+  select(VisitorId, everything())
+summary(hybrid2_accuracy_by_user$TP)
+
+# Obtain Top N predictions vs. actual purchases last 2-months ------------------
+hybrid2_test_recommendations <- data.frame(VisitorId = character(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec1 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec2 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec3 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec4 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec5 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec6 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec7 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec8 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec9 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec10 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec11 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rec12 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec13 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec14 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec15 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec16 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec17 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec18 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec19 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rec20 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating1 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating2 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating3 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating4 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating5 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating6 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating7 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating8 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating9 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating10 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating11 = integer(length = nrow(getData(train_scheme, "unknown"))), 
+                                   rating12 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating13 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating14 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating15 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating16 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating17 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating18 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating19 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   rating20 = integer(length = nrow(getData(train_scheme, "unknown"))),
+                                   stringsAsFactors=FALSE) 
+
+for(i in 1:nrow(getData(train_scheme, "unknown"))) {
+  hybrid2_test_recommendations$VisitorId[i] <- dimnames(getData(train_scheme, "unknown"))[[1]][i]
+  hybrid2_test_recommendations$rec1[i]  <- hybrid2_predictions@items[[i]][1]
+  hybrid2_test_recommendations$rec2[i]  <- hybrid2_predictions@items[[i]][2]
+  hybrid2_test_recommendations$rec3[i]  <- hybrid2_predictions@items[[i]][3]
+  hybrid2_test_recommendations$rec4[i]  <- hybrid2_predictions@items[[i]][4]
+  hybrid2_test_recommendations$rec5[i]  <- hybrid2_predictions@items[[i]][5]
+  hybrid2_test_recommendations$rec6[i]  <- hybrid2_predictions@items[[i]][6]
+  hybrid2_test_recommendations$rec7[i]  <- hybrid2_predictions@items[[i]][7]
+  hybrid2_test_recommendations$rec8[i]  <- hybrid2_predictions@items[[i]][8]
+  hybrid2_test_recommendations$rec9[i]  <- hybrid2_predictions@items[[i]][9]
+  hybrid2_test_recommendations$rec10[i] <- hybrid2_predictions@items[[i]][10]
+  hybrid2_test_recommendations$rec11[i] <- hybrid2_predictions@items[[i]][11]
+  hybrid2_test_recommendations$rec12[i] <- hybrid2_predictions@items[[i]][12]
+  hybrid2_test_recommendations$rec13[i] <- hybrid2_predictions@items[[i]][13]
+  hybrid2_test_recommendations$rec14[i] <- hybrid2_predictions@items[[i]][14]
+  hybrid2_test_recommendations$rec15[i] <- hybrid2_predictions@items[[i]][15]
+  hybrid2_test_recommendations$rec16[i] <- hybrid2_predictions@items[[i]][16]
+  hybrid2_test_recommendations$rec17[i] <- hybrid2_predictions@items[[i]][17]
+  hybrid2_test_recommendations$rec18[i] <- hybrid2_predictions@items[[i]][18]
+  hybrid2_test_recommendations$rec19[i] <- hybrid2_predictions@items[[i]][19]
+  hybrid2_test_recommendations$rec20[i] <- hybrid2_predictions@items[[i]][20]
+  hybrid2_test_recommendations$rating1[i] <- hybrid2_predictions@ratings[[i]][1]
+  hybrid2_test_recommendations$rating2[i] <- hybrid2_predictions@ratings[[i]][2]
+  hybrid2_test_recommendations$rating3[i] <- hybrid2_predictions@ratings[[i]][3]
+  hybrid2_test_recommendations$rating4[i] <- hybrid2_predictions@ratings[[i]][4]
+  hybrid2_test_recommendations$rating5[i] <- hybrid2_predictions@ratings[[i]][5]
+  hybrid2_test_recommendations$rating6[i] <- hybrid2_predictions@ratings[[i]][6]
+  hybrid2_test_recommendations$rating7[i] <- hybrid2_predictions@ratings[[i]][7]
+  hybrid2_test_recommendations$rating8[i] <- hybrid2_predictions@ratings[[i]][8]
+  hybrid2_test_recommendations$rating9[i] <- hybrid2_predictions@ratings[[i]][9]
+  hybrid2_test_recommendations$rating10[i] <- hybrid2_predictions@ratings[[i]][10]
+  hybrid2_test_recommendations$rating11[i] <- hybrid2_predictions@ratings[[i]][11]
+  hybrid2_test_recommendations$rating12[i] <- hybrid2_predictions@ratings[[i]][12]
+  hybrid2_test_recommendations$rating13[i] <- hybrid2_predictions@ratings[[i]][13]
+  hybrid2_test_recommendations$rating14[i] <- hybrid2_predictions@ratings[[i]][14]
+  hybrid2_test_recommendations$rating15[i] <- hybrid2_predictions@ratings[[i]][15]
+  hybrid2_test_recommendations$rating16[i] <- hybrid2_predictions@ratings[[i]][16]
+  hybrid2_test_recommendations$rating17[i] <- hybrid2_predictions@ratings[[i]][17]
+  hybrid2_test_recommendations$rating18[i] <- hybrid2_predictions@ratings[[i]][18]
+  hybrid2_test_recommendations$rating19[i] <- hybrid2_predictions@ratings[[i]][19]
+  hybrid2_test_recommendations$rating20[i] <- hybrid2_predictions@ratings[[i]][20]
+}
+
+# extract the item names from hybrid2_predictions object. Need to replace the 
+# predicted item indices with there obfuscation item label (line 371)
+item_labels <- as.character(hybrid2_predictions@itemLabels)
+item_labels_df <- data.frame(parent_index = 1:length(item_labels),
+                             parent_rec = item_labels)
+
+# convert top 20 rec's data from wide format to long format.
+top20_rec_tidy <- hybrid2_test_recommendations %>% 
+  select(-starts_with("rating")) %>% # drop predicted ratings columns
+  gather(key = rec_number, value = parent_index, -VisitorId) %>%
+  # strip text and keep the number.
+  mutate(rec_number = as.numeric(gsub("[^0-9]", "", rec_number))) %>%
+  # replace parent index with the actual item lable.
+  left_join(item_labels_df, by = 'parent_index') %>%
+  select(-parent_index)
+  
+top20_ratings_tidy <- hybrid2_test_recommendations %>% 
+  select(-starts_with("rec")) %>% # drop predicted records columns.
+  gather(key = rating_number, value = predicted_rating, -VisitorId) %>%
+  mutate(rec_number = as.numeric(gsub("[^0-9]", "", rating_number)))
+
+test_recommendations <- top20_rec_tidy %>%
+  inner_join(top20_ratings_tidy, by = c('VisitorId', 'rec_number')) %>%
+  select(-rating_number) %>%
+  rename(predicted_parent = parent_rec) %>%
+  arrange(VisitorId, rec_number)
+
+
+# CHECK: do values from the hybrid2_predictions S4 object for user1 match the parent
+# labels for the first user in the test_recommendations dataset.
+dimnames(getData(train_scheme, "unknown"))[[1]][1] # user "706937327430"
+user1_predicted_items <- hybrid2_predictions@items[[1]]
+user1_predicted_items <- hybrid2_predictions@itemLabels[user1_predicted_items]
+user1_predicted_items
+user1_predicted_ratings <- hybrid2_predictions@ratings[[1]]
+user1_predicted_ratings
+
+# data frame extraction predicted items match!
+all.equal(user1_predicted_items, 
+          as.character(test_recommendations[test_recommendations$VisitorId == '706937327430', ]$predicted_parent))
+
+# data frame extraction predicted ratings match!
+all.equal(user1_predicted_ratings, 
+          test_recommendations[test_recommendations$VisitorId == '706937327430', ]$predicted_rating)
+
+
